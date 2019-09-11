8c203a36c7df8cf83408af244e946f111a836cfc
Vladimir Troyanenko <post@itman.ru>
2019-09-11 22:25:41 +0200

    init

--- demo/src/app/_core/services/index.ts
+++
@@ -1,5 +0,0 @@
-// barrel
-export * from './_rest/notes/notes.service';
-export * from './_rest/interceptors/interceptor-spinner';
-export * from './error-handler/error-handler.service';
-export * from './spinner/spinner.service';
